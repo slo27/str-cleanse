aef23f6a37ff7850b66cde284c52ca368650f500
sandy lo <sandyks.lo27@gmail.com>
2022-11-18 22:34:58 -0800

    app component - quick rename

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+    # get /me
+    def show
+        render json: User.find(params[:id])
+    end
+
+    # post '/signup'
+    def create
+        
+    end
 end
